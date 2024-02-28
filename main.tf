05ac09758aac1a1bc8c7f03515eaa3c25922537f
SKIMRAN0509 <imran.shaik@clairvoyantsoft.com>
2024-02-28 17:20:41 +0530

    removed aggregate policy

--- main.tf
+++ main.tf
@@ -356,57 +356,49 @@ data "aws_iam_policy_document" "user_password_kms_permissions" {
 #   ]
 # }
 
-provider "aws" {
-  region = var.region
+# provider "aws" {
+#   region = var.region
 
-  # Make it faster by skipping the checks
-  skip_get_ec2_platforms      = true
-  skip_metadata_api_check     = true
-  skip_region_validation      = true
-  skip_credentials_validation = true
-  skip_requesting_account_id  = true
-}
+#   # Make it faster by skipping the checks
+#   skip_get_ec2_platforms      = true
+#   skip_metadata_api_check     = true
+#   skip_region_validation      = true
+#   skip_credentials_validation = true
+#   skip_requesting_account_id  = true
+# }
 
-data "aws_iam_policy_document" "resource_full_access" {
-  statement {
-    sid       = "FullAccess"
-    effect    = "Allow"
-    resources = ["arn:aws:s3:::bucketname/path/*"]
+# data "aws_iam_policy_document" "resource_full_access" {
+#   statement {
+#     sid       = "FullAccess"
+#     effect    = "Allow"
+#     resources = ["arn:aws:s3:::bucketname/path/*"]
 
-    actions = [
-      "s3:PutObject",
-      "s3:PutObjectAcl",
-      "s3:GetObject",
-      "s3:DeleteObject",
-      "s3:ListBucket",
-      "s3:ListBucketMultipartUploads",
-      "s3:GetBucketLocation",
-      "s3:AbortMultipartUpload"
-    ]
-  }
-}
+#     actions = [
+#       "s3:PutObject",
+#       "s3:PutObjectAcl",
+#       "s3:GetObject",
+#       "s3:DeleteObject",
+#       "s3:ListBucket",
+#       "s3:ListBucketMultipartUploads",
+#       "s3:GetBucketLocation",
+#       "s3:AbortMultipartUpload"
+#     ]
+#   }
+# }
 
-data "aws_iam_policy_document" "base" {
-  statement {
-    sid       = "BaseAccess"
-    effect    = "Allow"
-    resources = ["*"]
+# data "aws_iam_policy_document" "base" {
+#   statement {
+#     sid       = "BaseAccess"
+#     effect    = "Allow"
+#     resources = ["*"]
 
-    actions = [
-      "s3:ListBucket",
-      "s3:ListBucketVersions"
-    ]
-  }
-}
+#     actions = [
+#       "s3:ListBucket",
+#       "s3:ListBucketVersions"
+#     ]
+#   }
+# }
 
-module "aggregated_policy" {
-  source = "../../"
-
-  source_documents = [
-    data.aws_iam_policy_document.base.json,
-    data.aws_iam_policy_document.resource_full_access.json
-  ]
-}
 resource "aws_iam_role" "lambda" {
   count = var.enabled ? 1 : 0
 
