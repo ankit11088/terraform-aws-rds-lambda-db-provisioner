28f758a5a2a90f1067a757390abc8a54a5d655a3
SKIMRAN0509 <imran.shaik@clairvoyantsoft.com>
2024-02-28 17:17:00 +0530

    updated new changes

--- main.tf
+++ main.tf
@@ -339,23 +339,74 @@ data "aws_iam_policy_document" "user_password_kms_permissions" {
 #   ])
 # }
 
+# module "aggregated_policy" {
+#   source = "../../"
+
+# override_policy_documents = [
+#     # data.aws_iam_policy_document.base.json,
+#     # data.aws_iam_policy_document.resource_full_access.json,
+#     data.aws_iam_policy_document.default_permissions.json,
+#     data.aws_iam_policy_document.lambda_kms_permissions.json,
+#     data.aws_iam_policy_document.master_password_ssm_permissions.json,
+#     data.aws_iam_policy_document.master_password_kms_permissions.json,
+#     data.aws_iam_policy_document.master_password_secretsmanager_permissions.json,
+#     data.aws_iam_policy_document.user_password_ssm_permissions.json,
+#     data.aws_iam_policy_document.user_password_kms_permissions.json,
+#     data.aws_iam_policy_document.user_password_secretsmanager_permissions.json
+#   ]
+# }
+
+provider "aws" {
+  region = var.region
+
+  # Make it faster by skipping the checks
+  skip_get_ec2_platforms      = true
+  skip_metadata_api_check     = true
+  skip_region_validation      = true
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+}
+
+data "aws_iam_policy_document" "resource_full_access" {
+  statement {
+    sid       = "FullAccess"
+    effect    = "Allow"
+    resources = ["arn:aws:s3:::bucketname/path/*"]
+
+    actions = [
+      "s3:PutObject",
+      "s3:PutObjectAcl",
+      "s3:GetObject",
+      "s3:DeleteObject",
+      "s3:ListBucket",
+      "s3:ListBucketMultipartUploads",
+      "s3:GetBucketLocation",
+      "s3:AbortMultipartUpload"
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "base" {
+  statement {
+    sid       = "BaseAccess"
+    effect    = "Allow"
+    resources = ["*"]
+
+    actions = [
+      "s3:ListBucket",
+      "s3:ListBucketVersions"
+    ]
+  }
+}
+
 module "aggregated_policy" {
   source = "../../"
 
-  source_policy_documents = [
-    # data.aws_iam_policy_document.base.json,
-    # data.aws_iam_policy_document.resource_full_access.json,
-    data.aws_iam_policy_document.default_permissions.json,
-    data.aws_iam_policy_document.lambda_kms_permissions.json,
-    data.aws_iam_policy_document.master_password_ssm_permissions.json,
-    data.aws_iam_policy_document.master_password_kms_permissions.json,
-    data.aws_iam_policy_document.master_password_secretsmanager_permissions.json,
-    data.aws_iam_policy_document.user_password_ssm_permissions.json,
-    data.aws_iam_policy_document.user_password_kms_permissions.json,
-    data.aws_iam_policy_document.user_password_secretsmanager_permissions.json
+  source_documents = [
+    data.aws_iam_policy_document.base.json,
+    data.aws_iam_policy_document.resource_full_access.json
   ]
 }
-
 resource "aws_iam_role" "lambda" {
   count = var.enabled ? 1 : 0
 
